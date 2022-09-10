b14305a09399ff8dad377c8814a8a80d5123270d
B.Orgilbat <zettry1@gmail.com>
2022-09-10 10:06:37 -0500

    first

---
+++ lab4/src/main/java/lab4/Lab4Application.java
@@ -0,0 +1,14 @@
+package lab4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4Application {
+
+	public static void main(String[] args) {
+		System.out.println("Lab 4 oap");
+		SpringApplication.run(Lab4Application.class, args);
+	}
+
+}
